5924addfaaaa4f8ba7a197e3df1da9c9e9f4d3e6
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-10-26 18:56:32 +0100

    fix - Header z-index

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,14 @@ import { FileText } from "lucide-react"
 
 export default function Header() {
   return (
-    <div className="fixed flex h-20 bg-white w-full shadow">
+    <div className="fixed flex h-20 z-10 bg-white w-full shadow">
       <div className="flex items-center justify-between max-w-7xl mx-auto w-full px-4">
         <Link href={"/"} className="flex items-center space-x-1.5">
           <div className="h-12 w-12 rounded-full bg-violet-600 flex items-center justify-center">
             < FileText className="m-2 text-white" />
           </div>
           <h1 className="text-lg font-bold">LAPform</h1>
-        </Link> 
+        </Link>
         <Button variant={"outline"}>Se déconnecter</Button>
       </div>
     </div>
